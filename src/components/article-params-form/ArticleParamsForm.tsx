fd8983bba834272cceba4ab97bc8ea75f91ed9aa
smolinivan <smolin9667@gmail.com>
2025-03-30 02:33:10 +0300

    feat: Added functionality for applying and reseting settings

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,17 +1,128 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
-
+import { useEffect, useRef, useState } from 'react';
 import styles from './ArticleParamsForm.module.scss';
+import { Select } from 'src/ui/select';
+import { OptionType } from 'src/constants/articleProps';
+import { RadioGroup } from 'src/ui/radio-group/RadioGroup';
+import { Separator } from 'src/ui/separator';
+import { Text } from 'src/ui/text';
 
-export const ArticleParamsForm = () => {
+type ArticleSelectOptions = {
+	fontFamilies: OptionType[];
+	fontSizes: OptionType[];
+	fontColors: OptionType[];
+	backgroundColors: OptionType[];
+	contentWidths: OptionType[];
+};
+
+type ArticleSelectedOptions = {
+	fontFamily: OptionType;
+	fontSize: OptionType;
+	color: OptionType;
+	backgroundColor: OptionType;
+	contentWidth: OptionType;
+};
+
+type ArticleParamsProps = {
+	options: ArticleSelectOptions;
+	selected: ArticleSelectedOptions;
+	changeSetting: (value: OptionType, key: string) => void;
+	resetSetting?: () => void;
+	applySettings?: () => void;
+};
+
+export const ArticleParamsForm = (props: ArticleParamsProps) => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const settingsAside = useRef<HTMLElement | null>(null);
+
+	function toggleOpen() {
+		setIsOpen(!isOpen);
+		if (!isOpen) {
+			settingsAside.current?.classList.add(styles.container_open);
+		} else {
+			settingsAside.current?.classList.remove(styles.container_open);
+		}
+	}
+
+	function handleClickOutside(event: MouseEvent) {
+		if (
+			settingsAside.current?.classList.contains(styles.container_open) &&
+			!settingsAside.current.contains(event.target as Node)
+		) {
+			toggleOpen();
+		}
+	}
+
+	useEffect(() => {
+		document.addEventListener('mousedown', handleClickOutside);
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside);
+		};
+	}, [isOpen]);
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton isOpen={isOpen} onClick={toggleOpen} />
+			<aside className={styles.container} ref={settingsAside}>
+				<form
+					className={styles.form}
+					onSubmit={(e) => {
+						e.preventDefault();
+					}}>
+					<Text
+						children={'задайте параметры'}
+						size={31}
+						uppercase={true}
+						weight={800}></Text>
+					<Select
+						title='шрифт'
+						selected={props.selected.fontFamily}
+						options={props.options.fontFamilies}
+						onChange={(value) =>
+							props.changeSetting(value, 'fontFamilyOption')
+						}></Select>
+					<RadioGroup
+						selected={props.selected.fontSize}
+						options={props.options.fontSizes}
+						onChange={(value) => props.changeSetting(value, 'fontSizeOption')}
+						name='radio-box'
+						title='размер шрифта'></RadioGroup>
+					<Select
+						title='цвет шрифта'
+						selected={props.selected.color}
+						options={props.options.fontColors}
+						onChange={(value) =>
+							props.changeSetting(value, 'fontColor')
+						}></Select>
+					<Separator></Separator>
+					<Select
+						title='цвет фона'
+						selected={props.selected.backgroundColor}
+						options={props.options.backgroundColors}
+						onChange={(value) =>
+							props.changeSetting(value, 'backgroundColor')
+						}></Select>
+					<Select
+						selected={props.selected.contentWidth}
+						options={props.options.contentWidths}
+						onChange={(value) =>
+							props.changeSetting(value, 'contentWidth')
+						}></Select>
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
-						<Button title='Применить' htmlType='submit' type='apply' />
+						<Button
+							title='Сбросить'
+							htmlType='reset'
+							type='clear'
+							onClick={props.resetSetting}
+						/>
+						<Button
+							title='Применить'
+							htmlType='submit'
+							type='apply'
+							onClick={props.applySettings}
+						/>
 					</div>
 				</form>
 			</aside>
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,18 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
+import {
+	backgroundColors,
+	contentWidthArr,
+	defaultArticleState,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+	OptionType,
+} from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
@@ -13,19 +21,60 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	const [options, setOptions] = useState(defaultArticleState);
+
+	const [articleState, setArticleState] = useState(defaultArticleState);
+
+	function handleSelectSetting(selected: OptionType, key: string) {
+		setOptions((previousState) => ({
+			...previousState,
+			[key]: selected,
+		}));
+		console.log(selected);
+		console.log(options);
+	}
+
+	function handleSetArticleState() {
+		setArticleState(options);
+	}
+
+	function handleResetSettings() {
+		console.log('kek');
+		setOptions(defaultArticleState);
+		setArticleState(defaultArticleState);
+	}
+
 	return (
 		<main
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': articleState.fontFamilyOption.value,
+					'--font-size': articleState.fontSizeOption.value,
+					'--font-color': articleState.fontColor.value,
+					'--container-width': articleState.contentWidth.value,
+					'--bg-color': articleState.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm
+				options={{
+					fontFamilies: fontFamilyOptions,
+					fontSizes: fontSizeOptions,
+					fontColors: fontColors,
+					backgroundColors: backgroundColors,
+					contentWidths: contentWidthArr,
+				}}
+				selected={{
+					fontFamily: options.fontFamilyOption,
+					fontSize: options.fontSizeOption,
+					color: options.fontColor,
+					backgroundColor: options.backgroundColor,
+					contentWidth: options.contentWidth,
+				}}
+				changeSetting={handleSelectSetting}
+				resetSetting={handleResetSettings}
+				applySettings={handleSetArticleState}
+			/>
 			<Article />
 		</main>
 	);
